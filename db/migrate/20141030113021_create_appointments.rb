75db8bd9d7e87dbe818d9a178c42ac7d004f6af3
VinothiniBalakrishnan <vinothinibj@gmail.com>
2014-11-03 23:22:56 +0530

    First commit

---
+++ db/migrate/20141030113021_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration
+  def change
+    create_table :appointments do |t|
+      t.string :title
+      t.text :description
+      t.datetime :start_time
+      t.datetime :end_time
+
+      t.timestamps null: false
+    end
+  end
+end
